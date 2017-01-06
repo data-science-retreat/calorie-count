8c1cd22eb25ff26f95e44a8139079c59ae060790
danimateos <dmateos.sanmartin@gmail.com>
2017-01-06 18:38:37 +0100

    No more zombie processes

--- scripts/benchmark_class.sh
+++ scripts/benchmark_class.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
+
+cleanup() {
+    # Kill the background processes
+    kill $GPU_JOB
+    kill $CPU_JOB
+}
+
+trap cleanup SIGHUP SIGINT SIGTERM
+
 SBT_EXPR=$1
 REGEX="time: ([0-9]+) s, completed"
 
+
+
+
 # Start recording in background
 SCRIPTS_PATH=$( cd "$(dirname "$0")" ; pwd -P )
-"${SCRIPTS_PATH}/record_nvidia_usage.sh" 1 gpuasdf &
+"${SCRIPTS_PATH}/record_nvidia_usage.sh" 1 gpu &
 GPU_JOB=$!
-"${SCRIPTS_PATH}/record_cpu_usage.sh" 1 cpuasdf &
+"${SCRIPTS_PATH}/record_cpu_usage.sh" 1 cpu &
 CPU_JOB=$!
 
 # Change the IFS temporarily to split only on newlines:
@@ -20,18 +32,13 @@ do
     # =~ is the bash regex match operator:
     if [[ $line =~ $REGEX ]]
     then
-	echo matched
-	echo $line
 	TIME="${BASH_REMATCH[1]}"
-	echo $TIME
-	SUM_USAGE_GPU=$(tail -n $TIME gpuasdf | paste -s -d+ | bc)
-	SUM_USAGE_CPU=$(tail -n $TIME cpuasdf | paste -s -d+ | bc)	
+	SUM_USAGE_GPU=$(tail -n $TIME gpu | paste -s -d+ | bc)
+	SUM_USAGE_CPU=$(tail -n $TIME cpu | paste -s -d+ | bc)	
 	AVG_USAGE_GPU=$(expr $SUM_USAGE_GPU / $TIME)
-	AVG_USAGE_CPU=$(expr $SUM_USAGE_CPU / $TIME)	
+	AVG_USAGE_CPU=$(expr $SUM_USAGE_CPU / $TIME)
 	echo "This run took $TIME seconds with an average GPU % utilization of $AVG_USAGE_GPU and CPU % $AVG_USAGE_CPU"
     fi
 done
 
-# Kill the background processes
-kill $GPU_JOB
-kill $CPU_JOB
+cleanup
