6293ea53262dd783dcc1d42a8b6c887f4f527eb6
danimateos <dmateos.sanmartin@gmail.com>
2017-01-30 14:42:26 +0100

    Remove script for downloading the Ubuntu corpus

--- src/main/resources/scripts/download_workaround.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for ending in `echo aa ab ac ad ae`;
-do
-    wget "http://cs.mcgill.ca/~npow1/data/ubuntu_dataset.tgz.${ending}"
-done
-
-cat ubuntu_dataset.tgz.* > ubuntu_full_dataset.tgz
-tar -xvzf ubuntu_full_dataset.tgz data
